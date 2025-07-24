c66f58c8c30e378a2c9c4f9337ad4d22eccaa9e5
Wisdom Asigbey <113071078+WisdomAsigbey@users.noreply.github.com>
2025-07-24 17:19:52 +0300

    Add files via upload

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,30 @@
-import { getElement } from '../utils.js';
-import display from '../displayProducts.js';
-const setupSearch = () => {};
+import { getElement } from "../utils.js";
+import display from "../displayProducts.js";
+
+const form = getElement(".input-form");
+const inputValue = getElement(".search-input");
+
+const setupSearch = (store) => {
+  form.addEventListener("keyup", function () {
+    const value = inputValue.value;
+
+    if (value) {
+      const newStore = store.filter((products) => {
+        let { name } = products;
+        name = name.toLowerCase();
+        if (name.startsWith(value)) {
+          return products;
+        }
+      });
+      display(newStore, getElement(".products-container"));
+      if (newStore.length < 1) {
+        const productsDOM = getElement(".products-container");
+        productsDOM.innerHTML = `<h3 class = "filter-error">sorry, no products matched your search</h3>`;
+      }
+    } else {
+      display(store, getElement(".products-container"));
+    }
+  });
+};
 
 export default setupSearch;
